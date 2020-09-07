dd0cc78c676466ba990a8f6522ed61eb1357b78e
JohnsirK <1241010946@qq.com>
2020-09-07 14:12:28 +0800

    mixins_config

--- src/main.js
+++ src/main.js
@@ -12,42 +12,46 @@ import '@/assets/iconfont/iconfont.css'
 import storage from '@/utils/storage.js'
 // 国际化
 import i18n from './i18n/i18n'
-// 引入c3动画
+// 引入css3动画
 import animated from 'animate.css'
 // 按需引入需要使用的element
 import "@/utils/element.js";
 import 'element-ui/lib/theme-chalk/index.css'
+import VueRouter from 'vue-router'
 Vue.use(animated)
 Vue.config.productionTip = false
-// 跳转页面之后回到最顶部
-router.afterEach((to, from, next) => {
-  window.scrollTo(0, 0)
+
+router.afterEach(() => {
+	window.scrollTo(0, 0)
 })
+//跳转相同路由报错
+const originalPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push(location) {
+	return originalPush.call(this, location).catch(err => err)
+}
 
 // 判断用户是否登录,验证本地存储是否有token
 // 然后在router.js 页面给需要登录的路由页面添加一个meta
 router.beforeEach((to, from, next) => {
-  const type = to.meta.requiresAuth
-  // 判断该路由是否需要登录权限
-  if (type) {
-    if (storage.getLocal('userinfo') && storage.getLocal('userinfo').token) {
-      if(!storage.getLocal('userinfo').auth && to.meta.limit){
-        next('/login')
-      }else{
-        next()
-      }
-    } else {
-      storage.removeLocal('userinfo')
-      next('/login')
-    }
-  } else {
-    // 确保一定要有next()被调用
-    next()
-  }
+	const type = to.meta.requiresAuth
+	if (type) {
+		if (storage.getLocal('userinfo') && storage.getLocal('userinfo').token) {
+			if (!storage.getLocal('userinfo').auth && to.meta.limit) {
+				next('/login')
+			} else {
+				next()
+			}
+		} else {
+			storage.removeLocal('userinfo')
+			next('/login')
+		}
+	} else {
+		next()
+	}
 })
 new Vue({
-  router,
-  store,
-  i18n,
-  render: h => h(App)
+	router,
+	store,
+	i18n,
+	render: h => h(App)
 }).$mount('#app')
