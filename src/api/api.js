9acc493ba592d4f8a6ea699569c6f5b0a5dd2e75
IDeaL666 <2410649433@qq.com>
2020-09-08 11:25:08 +0800

    添加代理店管理接口

--- src/api/api.js
+++ src/api/api.js
@@ -6,62 +6,105 @@ import store from '../store'
  * @return {Promise} promise
  * */
 export const login = (data) => {
-    return server({
-        url:'/admin/login',
-        method: 'POST',
-        data
-    })
+  return server({
+    url: '/admin/login',
+    method: 'POST',
+    data
+  })
 }
 /**
  * @description: 获取管理员列表
  * @return {Promise} promise
  * */
 export const getManagementList = (params) => {
-    return server({
-        url:'/admin/user',
-        method: 'GET',
-        params
-    })
+  return server({
+    url: '/admin/user',
+    method: 'GET',
+    params
+  })
 }
 /**
  * @description: 添加管理员
  * @return {Promise} promise
  * */
 export const addManagement = (data) => {
-    return server({
-        url:'/admin/user',
-        method: 'POST',
-        data
-    })
+  return server({
+    url: '/admin/user',
+    method: 'POST',
+    data
+  })
 }
 /**
  * @description:编辑管理员
  * @return {Promise} promise
  * */
-export const editManagement = ({id,...data}) => {
-    return server({
-        url:`/admin/user/${id}`,
-        method: 'PUT',
-        data
-    })
+export const editManagement = ({ id, ...data }) => {
+  return server({
+    url: `/admin/user/${id}`,
+    method: 'PUT',
+    data
+  })
 }
 /**
  * @description: 删除管理员
  * @return {Promise} promise
  * */
 export const deleteManagement = (id) => {
-    return server({
-        url:`/admin/user/${id}`,
-        method: 'DELETE'
-    })
+  return server({
+    url: `/admin/user/${id}`,
+    method: 'DELETE'
+  })
 }
 /**
  * @description: 获取管理员详情
  * @return {Promise} promise
  * */
 export const getManagementDetail = (id) => {
-    return server({
-        url:`/admin/user/${id}`,
-        method: 'GET'
-    })
+  return server({
+    url: `/admin/user/${id}`,
+    method: 'GET'
+  })
 }
+/**
+ * @description: 获取blog列表
+ * @return {Promise} promise
+ * */
+export const getBlog = () => {
+  return server({
+    url: '/blog/getForm',
+    method: 'GET'
+  })
+}
+/**
+ * @description: 修改blog数据
+ * @return {Promise} promise
+ * */
+export const editBlog = (data) => {
+  return server({
+    url: '/blog/editForm',
+    method: 'POST',
+    data
+  })
+}
+/**
+ * @description: 添加blog数据
+ * @return {Promise} promise
+ * */
+export const addBlog = (data) => {
+  return server({
+    url: '/blog/addForm',
+    method: 'POST',
+    data
+  })
+}
+/**
+ * @description: 删除blog数据
+ * @return {Promise} promise
+ * */
+export const deleteBlog = (id) => {
+  return server({
+    url: `/blog/delForm/${id}`,
+    method: 'DELETE'
+  })
+}
+
